0fcdedd5ad2fab018faf0f5ef56a15cff613dc9b
Shomari Prince <support@d14na.org>
2018-10-09 23:35:35 -0400

    Fixed support for dotBit searches.

--- js/_handle0penMessage.js
+++ js/_handle0penMessage.js
@@ -33,6 +33,11 @@ const _handleAuth = function (_data) {
  * Handle SEARCH
  */
 const _handleSearch = function (_data) {
+    /* Validate data. */
+    if (_data && _data.dest && _data.innerPath) {
+        return _handleConfig(_data)
+    }
+
     /* Retrieve search result. */
     const body = _data.result
 
